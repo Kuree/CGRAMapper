9968cf88e7ac77dbea3d78511f23c7b6f3d1a779
Keyi Zhang <keyi@stanford.edu>
2019-05-27 19:24:43 -0700

    fix a bug

--- src/passes/constdup.h
+++ src/passes/constdup.h
@@ -41,7 +41,8 @@ void MapperPasses::ConstDuplication::setVisitorInfo() {
   Context* c = this->getContext();
   if (c->hasModule("corebit.const")) {
     addVisitorFunction(c->getModule("corebit.const"),ConstDup);
-  } else if (c->hasGenerator("coreir.const")) {
+  }
+  if (c->hasGenerator("coreir.const")) {
     addVisitorFunction(c->getGenerator("coreir.const"),ConstDup);
   }
 
